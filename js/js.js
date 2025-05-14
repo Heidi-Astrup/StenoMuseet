bbaf768243e2add388ab5589f4bbb3f6daad3f08
Annikahlaursen <Annikahlaursen@gmail.com>
2025-05-14 14:31:00 +0200

    færdig for i dag
    Footer er ordnet, mangler lige lidt

--- js/js.js
+++ js/js.js
@@ -1,4 +1,14 @@
-
+//HTML injektion til footer
+fod.innerHTML += `
+<div class="footer">
+    <div id="footerlinks">
+        <a id="farvefooter1" href="omos.html"><h3>Shops</h3></a>
+        <a id="farvefooter2" href="omos.html"><h3>Shops</h3></a>
+        <a id="farvefooter3" href="omos.html"><h3>Shops</h3></a>
+        <a id="farvefooter4" href="omos.html"><h3>Shops</h3></a>
+    </div>
+</div>
+`
 
 //HTML injektion hvor teksten på spørgsmålsiden bliver udskiftet med første spørgsmål og 3 svarmulgiheder
     sporsmal.innerHTML = `
